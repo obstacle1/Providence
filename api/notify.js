0f54a8cdc28f3162866a754d52e68e4ea5c23323
CVAULT <thomasgrayling@gmail.com>
2026-06-21 16:05:11 -0700

    Move API key to env var: api/notify.js

--- api/notify.js
+++ api/notify.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer re_8qwpPbzM_NZqPVDA6b7K78awjUJiMZ5iV`,
+        "Authorization": `Bearer ${process.env.RESEND_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
